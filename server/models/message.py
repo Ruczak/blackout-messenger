d69c86321b17b02859eb2ddc4c856d8221ac65fc
Ruczak <igor.sarnowski@gmail.com>
2023-04-19 10:54:57 +0200

    Polished message encoding and decoding

--- server/models/message.py
+++ server/models/message.py
@@ -8,7 +8,7 @@ class Message:
         self.receiver = receiver
 
         if len(args) == 1 and isinstance(args[0], bytes):
-            self.__decode()
+            self.__decode(args[0])
         elif len(args) == 2 and isinstance(args[0], str) and isinstance(args[1], float):
             self.message = args[0]
             self.sender_time = args[1]
@@ -16,8 +16,8 @@ class Message:
             raise ValueError("Arguments passed to the constructor do not match")
 
     def __decode(self, buffer: bytes):
-        self.sender_time = float(buffer[0:8])
-        self.message = buffer[8:].decode("utf-8")
+        (self.sender_time, ), text_buffer = struct.unpack("d", buffer[:8]), buffer[8:]
+        self.message = text_buffer.decode("utf-8")
 
     def encode(self):
         s = bytes(self.message, "utf-8")
