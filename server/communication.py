73f3db436b2e5cd317b05aad0e9788f10b920095
Ruczak <igor.sarnowski@gmail.com>
2023-04-19 12:37:50 +0200

    Adjusted communication class to message model

--- server/communication.py
+++ server/communication.py
@@ -1,35 +1,42 @@
 import asyncio
+import time
 from lora.sx126x import sx126x
-import struct
+from models.message import Message
+from collections.abc import Callable, Coroutine
 
 
-class Communication(sx126x):
-    def __init__(self, freq: int, address: int):
-        sx126x.__init__(self, serial_num="/dev/ttyS0", freq=freq, addr=address, power=22, rssi=False, air_speed=2400, relay=False)
+class Communication:
+    def __init__(self, serial_port: str, freq: int, address: int):
+        self.lora = sx126x(serial_num=serial_port, freq=freq, addr=address, power=22, rssi=False, air_speed=2400,
+                           relay=False)
 
     # send row in specified format by frequency to address
-    async def send(self, address: int, message: str):
+    async def send(self, address: int, message: str) -> Message:
         try:
-            offset_frequency = int(freq) - (850 if int(freq) > 850 else 410)
-            buffer = bytes(message, "utf-8")
-            data = bytes([int(address) >> 8]) + bytes([int(address) & 0xff]) + bytes([offset_frequency]) + bytes([self.addr >> 8]) + bytes([self.addr & 0xff]) + bytes([self.offset_freq]) + buffer
+            m = Message(self.lora.addr, address, message, time.time())
 
-            sx126x.send(self, data)
-            print(f"Sent data to {address} / {freq} MHz: {buffer.hex()}")
+            offset_frequency = int(self.lora.addr) - (850 if int(self.lora.freq) > 850 else 410)
+            buffer = m.encode()
+            data = bytes([int(address) >> 8]) + bytes([int(address) & 0xff]) + bytes([offset_frequency]) + bytes([
+                self.lora.addr >> 8]) + bytes([self.lora.addr & 0xff]) + bytes([self.lora.offset_freq]) + buffer
+
+            self.lora.send(data)
+            print(f"Sent data to {address} / {self.lora.freq} MHz: {buffer.hex()}")
+            return m
         except asyncio.CancelledError:
             print("Cancelled sending data.")
             raise
 
     # receive information from other device and execute handler
     # with address and message passed
-    async def receive(self, handler):
+    async def receive(self, handler: Callable[[Message], Coroutine]):
         try:
-            if self.ser.inWaiting() > 0:
+            if self.lora.ser.inWaiting() > 0:
                 await asyncio.sleep(0.5)
-                r_buff = self.ser.read(self.ser.inWaiting())
-                message = r_buff[6:].decode("utf-8")
-                address = int(r_buff[4])
-                handler(address, message)
+                r_buff = self.lora.ser.read(self.lora.ser.inWaiting())
+
+                m = Message(int(r_buff[4]), self.lora.addr, r_buff[6:])
+                handler(m)
         except asyncio.CancelledError:
             print("Cancelled receiving data")
             raise
--- server/models/message.py
+++ server/models/message.py
@@ -1,4 +1,3 @@
-import time
 import struct
 
 
@@ -10,15 +9,15 @@ class Message:
         if len(args) == 1 and isinstance(args[0], bytes):
             self.__decode(args[0])
         elif len(args) == 2 and isinstance(args[0], str) and isinstance(args[1], float):
-            self.message = args[0]
+            self.content = args[0]
             self.sender_time = args[1]
         else:
             raise ValueError("Arguments passed to the constructor do not match")
 
     def __decode(self, buffer: bytes):
         (self.sender_time, ), text_buffer = struct.unpack("d", buffer[:8]), buffer[8:]
-        self.message = text_buffer.decode("utf-8")
+        self.content = text_buffer.decode("utf-8")
 
     def encode(self):
-        s = bytes(self.message, "utf-8")
+        s = bytes(self.content, "utf-8")
         return struct.pack("d%ds" % (len(s),), self.sender_time, s)
