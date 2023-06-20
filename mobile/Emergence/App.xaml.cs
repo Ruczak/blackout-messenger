ce13012bffc67625dc257fb97002110dfae7ebf4
Ruczak <igor.sarnowski@gmail.com>
2023-06-20 11:53:17 +0200

    Added project files

---
+++ mobile/Emergence/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Emergence;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ mobile/Emergence/MessageTemplateSelector.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emergence
+{
+    public class MessageTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate IncomingTemplate { get; set; } 
+        public DataTemplate OutgoingTemplate { get; set; }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return ((Message)item).sender != MainPage.ServerID ? IncomingTemplate : OutgoingTemplate;
+        }
+    }
+}
